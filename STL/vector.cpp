a8e5f7114f63daa2c3ed694090318c0c1734ef95
sanjeevsaharaniiita <sanjeevsaharaniiita@gmail.com>
2020-05-05 00:46:56 +0530

    vector of vector

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -51,6 +51,19 @@ int main()
 	vector<string>vstr(6);
 	vector<int>vec[5]; //Creates 5 vectors of int type
 
+	vector< vector< int > > Matrix2(4, vector< int >(5, -1)); //create a matrix of size 4*5 and fill it with -1.
+
+
+	for (auto& vector_ : Matrix2) //print vector of vector
+	{
+		for (auto& i : vector_)
+		{
+			cout << i << " ";
+		}
+		cout << endl;
+	}
+
+
 	cout << "Capicity : " << v.capacity() << endl;
 	for (int i = 0; i < 5; i++)
 	{
@@ -69,6 +82,12 @@ int main()
 
 	v.push_back(100);
 
+	PrintVector(v);
+
+	v.erase(v.begin() + 4, v.begin() + 8);
+	PrintVector(v);
+
+	v.insert(v.begin()+6, 3);
 	PrintVector(v);
 
 	return 0;
